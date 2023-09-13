4b524bdea33b5e1deb12b4781091e5a9d3e2cb61
Ekaterina <e.a.dodonova@gmail.com>
2023-09-13 20:57:15 +0300

    Create apps api, reviews. Add basic models.

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,38 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model() # можно для начала так оставить
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Review(models.Model):
+    title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    pub_date = models.DateTimeField()
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
+class Comment(models.Model):
+    review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField()
+
+
+class Genre_Title(models.Model):
+    title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
+    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
