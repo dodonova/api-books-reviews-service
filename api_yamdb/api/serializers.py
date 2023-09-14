0830a43b263de9bbd00e34bcfc0f95fa2db837ea
Aleksei Kotko <akotko@yandex.ru>
2023-09-14 14:55:06 +0300

    Созданы пути в urls.py, view-функции и сер

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,29 @@
+from rest_framework import serializers
+
+from reviews.models import Review, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comment
+        read_only_fields = ('author', 'review_id',)
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Review
+        read_only_fields = ('author', 'title_id',)
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from api.serializers import (
+    ReviewSerializer,
+    CommentSerializer,
+)
+
+from reviews.models import Review, Comment, Title
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    # permission_classes = (AuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        title_id = self.kwargs.get("title_id")
+        serializer.save(
+            author=self.request.user, title_id=Title.objects.get(id=title_id))
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        new_queryset = Review.objects.filter(title_id=title_id)
+        return new_queryset
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    # permission_classes = (AuthorOrReadOnly,)
+
+    def perform_create(self, serializer):
+        review_id = self.kwargs.get("review_id")
+        serializer.save(
+            author=self.request.user,
+            review_id=Review.objects.get(id=review_id)
+        )
+
+    def get_queryset(self):
+        title_id = self.kwargs.get("title_id")
+        review_id = self.kwargs.get("review_id")
+        new_queryset = Comment.objects.filter(
+            title_id=title_id, review_id=review_id
+        )
+        return new_queryset
