09154048d413d85f9df9f8fe66c5f9342766a13e
Aleksei Kotko <akotko@yandex.ru>
2023-09-14 13:29:12 +0300

    Ветка для разработки: отзывы, комменты, рейтинги

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-User = get_user_model() # можно для начала так оставить
+User = get_user_model()  # можно для начала так оставить
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
 
+
 class Title(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
 
 class Review(models.Model):
     title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
