38603b6b692c40e96afc663f0e42d5301ee8c5ac
Ekaterina <e.a.dodonova@gmail.com>
2023-09-16 13:05:56 +0300

    Import tables from csv with bash

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,17 +1,25 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-User = get_user_model() # можно для начала так оставить
+User = get_user_model()  # можно для начала так оставить
 
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
 
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+
 class Title(models.Model):
     name = models.CharField(max_length=200)
     year = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre, related_name='genres')
+
 
 class Review(models.Model):
     title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
@@ -21,18 +29,8 @@ class Review(models.Model):
     pub_date = models.DateTimeField()
 
 
-class Genre(models.Model):
-    name = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-
-
 class Comment(models.Model):
     review_id = models.ForeignKey(Review, on_delete=models.CASCADE)
     text = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     pub_date = models.DateTimeField()
-
-
-class Genre_Title(models.Model):
-    title_id = models.ForeignKey(Title, on_delete=models.CASCADE)
-    genre_id = models.ForeignKey(Genre, on_delete=models.CASCADE)
